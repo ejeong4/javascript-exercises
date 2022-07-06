f4218aabb2ae4b9bd845d2cd13df0fa034486590
ejeong4 <mlej530@gmail.com>
2022-07-06 12:50:04 -0400

    Solve hellWorld and attempt repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(greeting, n) {
+    let multiply = greeting * n;
+    return String(multiply);
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;

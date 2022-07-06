30aebd067f7ef613a30c76faa616f2ac6cdb58c8
ejeong4 <mlej530@gmail.com>
2022-07-06 14:26:36 -0400

    Unskip remaining tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,15 @@
 const reverseString = function(word) {
     let apartString = [];
-    let stringLength = word.length;
 
-    for (i = stringLength - 1; i >= 0; i--) {
+    for (i = word.length - 1; i >= 0; i--) {
         apartString.push(word[i]);
     }
     return apartString.join('');
 
 };
-reverseString('hello')
-    // Do not edit below this line
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
+// Do not edit below this line
 module.exports = reverseString;

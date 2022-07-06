8c6d7fad8fb6e35cf3b61ff189fb8b973edab865
ejeong4 <mlej530@gmail.com>
2022-07-06 13:15:27 -0400

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,22 @@
+let number = Math.floor(Math.random() * 1000);
 const repeatString = function(greeting, n) {
-    let multiply = greeting * n;
-    return String(multiply);
+
+    if (n < 0) {
+        return "ERROR";
+    } else {
+        let multiply = greeting.repeat(n);
+        return multiply;
+    }
+
 };
 
 repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', number).match(/((hey))/g).length;
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;

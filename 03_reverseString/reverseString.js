71412298d6b9cdbf7e6a0078dc5dae6f873acd2a
ejeong4 <mlej530@gmail.com>
2022-07-06 14:16:20 -0400

    Solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let apartString = [];
+    let stringLength = word.length;
+
+    for (i = stringLength - 1; i >= 0; i--) {
+        apartString.push(word[i]);
+    }
+    return apartString.join('');
 
 };
-
-// Do not edit below this line
+reverseString('hello')
+    // Do not edit below this line
 module.exports = reverseString;

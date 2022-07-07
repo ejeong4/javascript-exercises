c72798ca91f78d6f959f367ef09cfc365d67ee1f
ejeong4 <mlej530@gmail.com>
2022-07-07 19:17:18 -0400

    Pass all sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,18 @@
 const sumAll = function(a, b) {
     let array = [];
-    for (i = a; i <= b; i++) {
-        a = i + 1;
-        array.push(i);
+    if (b < 0 || a < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    else if (a < b) {
+        for (i = a; i <= b; i++) {
+            a = i + 1;
+            array.push(i);
+        }
+    } else if (b < a) {
+        for (i = b; i <= a; i++) {
+            b = i + 1;
+            array.push(i);
+        }
+    } else {
+        return "ERROR";
     }
     let c = 0;
     for (i = 0; i < array.length; i++) {

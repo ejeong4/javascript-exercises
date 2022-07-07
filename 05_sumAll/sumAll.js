ad3533f9b5d7ff0bac0a7fe42d119c57e5dc4dd3
ejeong4 <mlej530@gmail.com>
2022-07-07 15:02:52 -0400

    Pass sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let array = [];
+    for (i = a; i <= b; i++) {
+        a = i + 1;
+        array.push(i);
+    }
+    let c = 0;
+    for (i = 0; i < array.length; i++) {
+        c += array[i];
+    }
+    return c;
 };
 
 // Do not edit below this line

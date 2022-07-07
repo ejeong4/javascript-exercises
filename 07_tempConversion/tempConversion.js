b7fdee93425946f70a691e30c6df5c901acc4aac
ejeong4 <mlej530@gmail.com>
2022-07-07 19:55:56 -0400

    Successfully run temp functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    let newTemp = ((temp - 32) * (5 / 9));
+    let cTemp = Number(newTemp.toFixed(1));
+    return cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let newTemp = ((temp * (9 / 5)) + 32);
+    let fTemp = Number(newTemp.toFixed(1));
+    return fTemp;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };

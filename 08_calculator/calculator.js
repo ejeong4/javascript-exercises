e39cd1f7dca6440cb92361c353d4fa91cf0daa10
ejeong4 <mlej530@gmail.com>
2022-07-20 18:37:22 -0400

    Add calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,30 @@
-const add = function() {
-	
-};
+const add = (arg1, arg2) => arg1 + arg2;
 
-const subtract = function() {
-	
-};
+const subtract = (arg1, arg2) => arg1 - arg2;
 
-const sum = function() {
-	
-};
+const sum = array => array.reduce((prev, next) => prev + next, 0);
 
-const multiply = function() {
 
-};
+const multiply = array => array.reduce((prev, next) => prev * next);
 
-const power = function() {
-	
-};
+const power = (prev, next) => prev ** next;
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+    let newNum = 1;
+    if (num === 0) return 1;
+
+    for (i = num; i > 0; i--) {
+        newNum *= i;
+    };
+    return newNum;
+}
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };

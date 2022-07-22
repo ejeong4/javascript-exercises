6fd201966e2d2ae9d2487f5851ce98318de4e86a
ejeong4 <mlej530@gmail.com>
2022-07-22 18:46:24 -0400

    Pass palindromes tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function(input) {
+    const word = input.toLowerCase().replace(/[^a-z]/g, '');
+    let newWord = "";
+    for (i = word.length - 1; i >= 0; i--) {
+        newWord += word[i];
+    }
+    return newWord === word ? true : false;
 };
 
 // Do not edit below this line

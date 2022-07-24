991bacb73a42b53d4e437f78527a67bf4ca21422
ejeong4 <mlej530@gmail.com>
2022-07-24 09:32:59 -0400

    Fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    let y = 0;
+    let x = 1;
+    for (i = 1; i < n; i++) {
+        const temp = x;
+        x = y + x;
+        y = temp;
+    }
+    return x;
 };
 
 // Do not edit below this line
